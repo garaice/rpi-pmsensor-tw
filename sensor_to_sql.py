ec5e1a8d4e7c00809b835a3d37a7e581d21b231f
garaice <gara_1@hotmail.com>
2019-01-28 14:31:14 +0100

    Update sensor_to_sql.py

--- sensor_to_sql.py
+++ sensor_to_sql.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #  
 #  Copyright 2017 Dr. M. Luetzelberger <webmaster@raspberryblog.de>
-#  
+#  https://github.com/luetzel
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
