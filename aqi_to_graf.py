77f0bfff5e54999e8a43c00a14037cd97f5abf96
garaice <gara_1@hotmail.com>
2019-02-27 15:22:39 +0100

    Update aqi_to_graf.py

--- aqi_to_graf.py
+++ aqi_to_graf.py
@@ -22,71 +22,55 @@ c = conn.cursor()
 ahora = datetime.now().strftime("%Y-%m-%d")
 hoy = "'"+ahora+"%';"
 
-sql = "SELECT hora, pm25, pm10 FROM data where hora like "+hoy
+sql = "SELECT hora, aqi FROM data where hora like "+hoy
 c.execute(sql)
 data = c.fetchall()
 
 hora = []
-pm25 = []
-pm10 = []
+aqi = []
 
 for row in data:
     hora.append(datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f'))#con el fin de pasar el texto a fecha
-    pm25.append(row[1])
-    pm10.append(row[2])
+    aqui.append(row[1])
         
 
 c.close()
 conn.close()
+
 indicehora = datetime.now()
-pm25hora = []
-pm25mediah = []
-pm25mediac = []
-pm10mediah = []
-pm10mediac = []
+aqihora = []
+aqimediah = []
+aqimediac = []
 media = []
 indice = 0
 valormedio = 0
 for x in hora:
     if x.hour <= indicehora.hour:
         indicehora = x
-        media.append(pm25[indice])
+        media.append(aqi[indice])
         indice = indice + 1
     else:
         valormedio = statistics.median(media)
-        pm25mediac.append(valormedio)
-        pm25mediah.append(indicehora)
+        aqimediac.append(valormedio)
+        aqimediah.append(indicehora)
         media = []
         indicehora = x
         indice= indice + 1
 media = []
 indice = 0       
-for x in hora:
-    if x.hour <= indicehora.hour:
-        indicehora = x
-        media.append(pm10[indice])
-        indice = indice + 1
-    else:
-        valormedio = statistics.median(media)
-        pm10mediac.append(valormedio)
-        pm10mediah.append(indicehora)
-        media = []
-        indicehora = x
-        indice= indice + 1            
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
-ax1.set_title("Particulas PM2.5 y PM10\nDurango "+ahora)
+ax1.set_title("AQI EPA\nDurango "+ahora)
 
    # Configure x-ticks
 ax1.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
 
    # Plot temperature data on left Y axis
-ax1.set_ylabel("PM2.5")
-ax1.plot_date(hora, pm25, '-', label="PM25>20 Peligro", color='g')
-ax1.plot_date(hora, pm10, '-', label="PM10>35 Peligro", color='#95d0fc')
-ax1.plot_date(pm25mediah, pm25mediac,'-', label="PM25 Media", color='r')
-ax1.plot_date(pm10mediah, pm10mediac,'-', label="PM10 Media", color='#01153e')
+ax1.set_ylabel("AQI")
+ax1.plot_date(hora, aqi, '-', label="AQI>20 Peligro", color='g')
+ax1.plot_date(aqimediah, aqimediac,'-', label="AQI Media", color='g')
+
    
 
    # Format the x-axis for dates (label formatting, rotation)
